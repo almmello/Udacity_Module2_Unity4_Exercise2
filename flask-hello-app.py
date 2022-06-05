fe524769610d680189e07e0c090ab8d9ff013ec0
Alexandre <almmello@gmail.com>
2022-06-05 18:14:11 -0300

    Exercise2 OK

--- flask-hello-app.py
+++ flask-hello-app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from urllib.parse import quote  
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://almmello@localhost:5432/example'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "Hello World!"
+    return 'Hello World!'
